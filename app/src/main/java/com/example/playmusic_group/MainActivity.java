f0f5d271bdaf8ac3bbb75ace157514a0f129003e
diennguyendhv <147355129+diennguyendhv@users.noreply.github.com>
2023-10-09 11:33:33 +0700

    Đã update lại âm nhạc và button

--- app/src/main/java/com/example/playmusic_group/MainActivity.java
+++ app/src/main/java/com/example/playmusic_group/MainActivity.java
@@ -28,6 +28,45 @@ public class MainActivity extends AppCompatActivity {
         AnhXa();
         AddBaiHat();
         PlayMusic();
+        btnPre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index --;
+                if (index < 0){
+                    index = arrayBaiHat.size() - 1;
+                }
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                }
+                PlayMusic();
+                mediaPlayer.start();
+                btnPlay.setImageResource(R.drawable.pause);
+            }
+        });
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index ++;
+                if(index > arrayBaiHat.size() - 1){
+                    index = 0;
+                }
+                if(mediaPlayer.isPlaying()){
+                    mediaPlayer.stop();
+                }
+                PlayMusic();
+                mediaPlayer.start();
+                btnPlay.setImageResource(R.drawable.pause);
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.stop();
+                mediaPlayer.release();
+                btnPlay.setImageResource(R.drawable.play);
+                PlayMusic();
+            }
+        });
         btnPlay.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -49,13 +88,19 @@ public class MainActivity extends AppCompatActivity {
 
     private void AddBaiHat() {
         arrayBaiHat = new ArrayList<>();
-        arrayBaiHat.add(new BaiHat("Anh Ba Khía Miền Tây", R.raw.anh_ba_khia));
-        arrayBaiHat.add(new BaiHat("Kiếp Ve Sầu", R.raw.khiep_ve_sau));
-        arrayBaiHat.add(new BaiHat("Mối Tình Đầu", R.raw.moi_tinh));
-        arrayBaiHat.add(new BaiHat("Môt Tình Yêu", R.raw.mot_tinh_yeu));
-        arrayBaiHat.add(new BaiHat("Mưa Cuộc Tình", R.raw.mua_cuoc_tinh));
-        arrayBaiHat.add(new BaiHat("Phôi Phai",R.raw.phoi_phai));
-        arrayBaiHat.add(new BaiHat("Tình Khúc", R.raw.tinh_khuc));
+        arrayBaiHat.add(new BaiHat("Anh Ba Khía Miền Tây - Đan Trường", R.raw.anh_ba_khia));
+        arrayBaiHat.add(new BaiHat("Kiếp Ve Sầu - Đan Trường", R.raw.kiep_ve_sau));
+        arrayBaiHat.add(new BaiHat("Mưa Trên Cuộc Tình - Đan Trường", R.raw.mua_tren_cuoc_tinh));
+        arrayBaiHat.add(new BaiHat("Nếu Phôi Pha Ngày Mai - Đan Trường",R.raw.phoi_phai));
+        arrayBaiHat.add(new BaiHat("Anh Đã Quen Với Cô Đơn - Sobin Hoàng Sơn",R.raw.anh_da_quen_voi_co_don);
+        arrayBaiHat.add(new BaiHat("Buông Đôi Tay Nhau Ra - Sơn Tùng MTP",R.raw.buong_doi_tay_nhau_ra);
+        arrayBaiHat.add(new BaiHat("Có Chàng Trai Viết Lên Cây",R.raw.co_chang_trai_viet_len_cay);
+        arrayBaiHat.add(new BaiHat("Gửi Anh Xa Nhớ - Bích Phương ",R.raw.gui_anh_xa_nho);
+        arrayBaiHat.add(new BaiHat("Như Phút Ban Đầu - Noo Phước Thịnh",R.raw.nhu_phut_ban_dau);
+        arrayBaiHat.add(new BaiHat("Việt Nam Những Chuyến Đi - Vicky Nhung",R.raw.viet_nam_va_nhung_chuyen_di);
+        arrayBaiHat.add(new BaiHat("Vợ Người Ta - Phan Mạnh Quỳnh",R.raw.vo_nguoi_ta);
+        arrayBaiHat.add(new BaiHat("Yêu Một Người Vô Tâm",R.raw.yeu_mot_nguoi_vo_tam);
+        arrayBaiHat.add(new BaiHat("Yêu Nhau Nửa Ngày",R.raw.yeu_nhau_nua_ngay);
     }
 
     private void AnhXa() {
