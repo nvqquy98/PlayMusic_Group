9c050826a636eff5ed9abfeb4b31b551542aa648
diennguyendhv <147355129+diennguyendhv@users.noreply.github.com>
2023-10-11 17:38:14 +0700

    Thêm chức năng ngẫu nhiên và lặp lại

--- app/src/main/java/com/example/playmusic_group/MainActivity.java
+++ app/src/main/java/com/example/playmusic_group/MainActivity.java
@@ -16,19 +16,20 @@ import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<BaiHat> arrayBaiHat;
-
     TextView txtTitle, txtCasi, txtTimeStart, txtTimeEnd;
     SeekBar skBar;
-    ImageButton btnPre, btnPlay, btnNext;
+    ImageButton btnPre, btnPlay, btnNext, btnTron, btnLap;
     ImageView music_compact;
     MediaPlayer mediaPlayer;
     Animation animation;
     int index = 0;
-
-
+    int lap = 0;
+    int tron = 0;
+    Random random = new Random();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,7 +74,6 @@ public class MainActivity extends AppCompatActivity {
                 UpdateTime();
             }
         });
-
         btnPlay.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -86,8 +86,33 @@ public class MainActivity extends AppCompatActivity {
                     btnPlay.setImageResource(R.drawable.pause);
                     music_compact.startAnimation(animation);
                 }
+                mediaPlayer.setLooping(false);
                 SetTimeEnd();
                 UpdateTime();
+            }
+        });
+        btnLap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                lap += 1;
+                if (lap % 3 == 1){
+                    btnLap.setImageResource(R.drawable.lap_on);
+                } else if (lap % 3 == 2) {
+                    btnLap.setImageResource(R.drawable.lap_mot);
+                } else {
+                    btnLap.setImageResource(R.drawable.lap_off);
+                }
+            }
+        });
+        btnTron.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tron += 1;
+                if(tron % 2 == 1){
+                    btnTron.setImageResource(R.drawable.tron_bai_on);
+                }else {
+                    btnTron.setImageResource(R.drawable.tron_bai_off);
+                }
             }
         });
         skBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
@@ -107,6 +132,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     private void UpdateTime(){
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -118,18 +144,69 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                     @Override
                     public void onCompletion(MediaPlayer mp) {
-                        index ++;
-                        if(index > arrayBaiHat.size() - 1){
-                            index = 0;
+                        if(lap % 3 == 1 && tron % 2 == 0){
+                            index ++;
+                            if(index > arrayBaiHat.size() - 1){
+                                index = 0;
+                            }
+                            if(mediaPlayer.isPlaying()){
+                                mediaPlayer.stop();
+                            }
+                            PlayMusic();
+                            mediaPlayer.start();
+                            btnPlay.setImageResource(R.drawable.pause);
+                            SetTimeEnd();
+                            UpdateTime();
                         }
-                        if(mediaPlayer.isPlaying()){
-                            mediaPlayer.stop();
+                        if(lap % 3 == 1 && tron % 2 == 1){
+                            index = random.nextInt(arrayBaiHat.size());
+                            if(index > arrayBaiHat.size() - 1){
+                                index = 0;
+                            }
+                            if(mediaPlayer.isPlaying()){
+                                mediaPlayer.stop();
+                            }
+                            PlayMusic();
+                            mediaPlayer.start();
+                            btnPlay.setImageResource(R.drawable.pause);
+                            SetTimeEnd();
+                            UpdateTime();
                         }
-                        PlayMusic();
-                        mediaPlayer.start();
-                        btnPlay.setImageResource(R.drawable.pause);
-                        SetTimeEnd();
-                        UpdateTime();
+                        if(lap % 3 == 2 && tron % 2 == 0){
+                            mediaPlayer.start();
+                        }
+                        if(lap % 3 == 2 && tron % 2 == 1){
+                            index = random.nextInt(arrayBaiHat.size());
+                            if(index > arrayBaiHat.size() - 1){
+                                index = 0;
+                            }
+                            if(mediaPlayer.isPlaying()){
+                                mediaPlayer.stop();
+                            }
+                            PlayMusic();
+                            mediaPlayer.start();
+                            btnPlay.setImageResource(R.drawable.pause);
+                            SetTimeEnd();
+                            UpdateTime();
+                        }
+                        if(lap % 3 == 0 && tron % 2 == 0){
+                            btnPlay.setImageResource(R.drawable.play);
+                            music_compact.clearAnimation();
+                        }
+                        if(lap % 3 == 0 && tron % 2 == 1){
+                            index = random.nextInt(arrayBaiHat.size());
+                            if(index > arrayBaiHat.size() - 1){
+                                index = 0;
+                            }
+                            if(mediaPlayer.isPlaying()){
+                                mediaPlayer.stop();
+                            }
+                            PlayMusic();
+                            mediaPlayer.start();
+                            btnPlay.setImageResource(R.drawable.pause);
+                            SetTimeEnd();
+                            UpdateTime();
+                        }
                     }
                 });
                 handler.postDelayed(this, 500);
@@ -141,13 +218,11 @@ public class MainActivity extends AppCompatActivity {
         txtTimeEnd.setText(time.format(mediaPlayer.getDuration()));
         skBar.setMax(mediaPlayer.getDuration());
     }
-
     private void PlayMusic() {
         mediaPlayer = MediaPlayer.create(MainActivity.this, arrayBaiHat.get(index).getFile());
         txtTitle.setText(arrayBaiHat.get(index).getTenBaiHat());
         txtCasi.setText(arrayBaiHat.get(index).getTenCaSi());
     }
-
     private void AddBaiHat() {
         arrayBaiHat = new ArrayList<>();
         arrayBaiHat.add(new BaiHat("Việt Nam Những Chuyến Đi","Vicky Nhung",R.raw.viet_nam_va_nhung_chuyen_di));
@@ -164,7 +239,6 @@ public class MainActivity extends AppCompatActivity {
         arrayBaiHat.add(new BaiHat("Yêu Một Người Vô Tâm","Bảo Anh",R.raw.yeu_mot_nguoi_vo_tam));
         arrayBaiHat.add(new BaiHat("Yêu Nhau Nửa Ngày","Phan Mạnh Quỳnh",R.raw.yeu_nhau_nua_ngay));
     }
-
     private void AnhXa() {
         txtTitle        = (TextView) findViewById(R.id.textViewTitle);
         txtCasi         = (TextView) findViewById(R.id.textViewSinger);
@@ -174,6 +248,8 @@ public class MainActivity extends AppCompatActivity {
         btnPre          = (ImageButton) findViewById(R.id.buttonPre);
         btnPlay         = (ImageButton) findViewById(R.id.buttonPlay);
         btnNext         = (ImageButton) findViewById(R.id.buttonNext);
+        btnTron         = (ImageButton) findViewById(R.id.buttonNgauNhien);
+        btnLap          = (ImageButton) findViewById(R.id.buttonLap);
         music_compact   = (ImageView) findViewById(R.id.musicCompact);
 
     }
